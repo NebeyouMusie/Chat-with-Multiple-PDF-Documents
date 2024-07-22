03986f13fb131cd36d64c74112a7420ea48d2c17
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-07-22 13:11:29 +0000

    update file

--- app/app.py
+++ app/app.py
@@ -1,6 +1,52 @@
 import streamlit as st
-from utils import user_input, get_pdf_text, get_text_chunk, get_vector_store, get_conversational_chain
+from langchain_core.messages import AIMessage, HumanMessage
+from utils import user_input, get_pdf_text, get_text_chunks, get_vector_store, get_conversational_chain
 
-st.set_page_config(page_title="ChatPDF")
+st.set_page_config(page_title="ChatPDF", page_icon='📃')
 
 st.header("Chat with Multiple PDF Documents")
+
+# nitialize session state
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = [
+        AIMessage(content="Hello there👋, I can help you with your PDF's. Upload any PDF and we can chat.")
+    ]
+
+# Display chat history
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message('AI'):
+            st.info(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+# accept user input
+user_question = st.chat_input("start chatting with your pdf")
+if user_question is not None and user_question != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_question))
+    
+    with st.chat_message("Human"):
+        st.markdown(user_question)
+        
+    response = user_input(user_question, st.session_state.chat_history)
+    
+    # Remove any unwanted prefixes from the response
+    response = response.replace("AI response:", "").replace("chat response:", "").replace("bot response:", "").strip()
+ 
+    with st.chat_message("AI"):
+        st.write(response)
+        
+    st.session_state.chat_history.append(AIMessage(content=response))
+
+
+with st.sidebar:
+    st.title("Menu")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", type="pdf", accept_multiple_files=True)
+    if st.button("Submit & Process"):
+        with st.spinner("Processing"):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            get_vector_store(text_chunks)
+            st.success("Done")
+
--- app/utils.py
+++ app/utils.py
@@ -2,9 +2,12 @@ from config import get_google_api_key, load_config
 import google.generativeai as genai
 from PyPDF2 import PdfReader
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings 
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 
 
@@ -22,8 +25,8 @@ def get_pdf_text(pdf_docs):
     return text
 
 # function to chunk pdf text
-def get_text_chunk(text):
-    text_splitter = RecursiveChraracterTextSplitter(chunk_size=1000, chunk_overlap=500)
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
     chunks = text_splitter.split_text(text)
     return chunks
 
@@ -31,13 +34,14 @@ def get_text_chunk(text):
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embeddings)
-    vector_store.save_local("Chat-with-Multiple-PDF-Documents/faiss_index")
+    vector_store.save_local("faiss_index")
    
 # function to setup model and chain 
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Chat history: \n{chat_history}\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
@@ -46,19 +50,19 @@ def get_conversational_chain():
      
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.5)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    chain = prompt | model
     
     return chain
 
 
-def user_input(user_question):
+def user_input(user_question, chat_history):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")    
     
-    new_db = FAISS.load_local("Chat-with-Multiple-PDF-Documents/")
+    new_db = FAISS.load_local("faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
     
-    chain = get_conversational_chain()
+    chain = get_conversational_chain() | StrOutputParser()
     
-    response = chain.invoke({"input_documents":docs, "question":user_question}, return_only_outputs=True)
+    response = chain.invoke({"chat_history":chat_history, "context":docs, "question":user_question})
     
     return response
